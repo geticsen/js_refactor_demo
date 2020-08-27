b777c3377778b7d4c50fc1c21b64015f53d05d03
ZHOUGR2 <286348794@qq.com>
2020-08-27 14:37:35 +0800

    [Refactor] [Green] test:add test case

--- test/statementTest.js
+++ test/statementTest.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const {statement} = require('../src/statement');
+const { statement } = require('../src/statement');
 const invoice = {
   'customer': 'BigCo',
   'performances': [
@@ -32,31 +32,65 @@ const plays = {
     'name': 'Othello',
     'type': 'tragedy',
   },
+  'exception': {
+    'name': 'exception test',
+    'type': 'exception',
+  },
 };
 
 
 test('statement case 1:Coustomer BigCo without performance', t => {
   const invoice = {
-    'customer':'BigCo',
-    'performances':[],
+    'customer': 'BigCo',
+    'performances': [],
   }
-  const result = statement(invoice,plays)
-  t.is(result,'Statement for BigCo\nAmount owed is $0.00\nYou earned 0 credits \n')
+  const result = statement(invoice, plays)
+  t.is(result, 'Statement for BigCo\nAmount owed is $0.00\nYou earned 0 credits \n')
 });
 
 test('statement case 1:Coustomer BigCo with as-like and audience 20', t => {
   const invoice = {
-    'customer':'BigCo',
-    'performances':[
+    'customer': 'BigCo',
+    'performances': [
       {
         'playID': 'as-like',
         'audience': 20,
       },
     ],
   }
-  const result = statement(invoice,plays)
-  console.log(result);
-  t.is(result,'Statement for BigCo\n As You Like It: $360.00 (20 seats)\nAmount owed is $360.00\nYou earned 4 credits \n')
+  const result = statement(invoice, plays)
+  t.is(result, 'Statement for BigCo\n As You Like It: $360.00 (20 seats)\nAmount owed is $360.00\nYou earned 4 credits \n')
+});
+test('statement case 1:Coustomer BigCo with othello and audience 30', t => {
+  const invoice = {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'othello',
+        'audience': 30,
+      },
+    ],
+  }
+  const result = statement(invoice, plays)
+  console.log(result)
+  t.is(result, 'Statement for BigCo\n Othello: $400.00 (30 seats)\nAmount owed is $400.00\nYou earned 0 credits \n')
+});
+test('statement case 1:Coustomer BigCo with exception', t => {
+  const invoice = {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'exception',
+        'audience': 20,
+      },
+    ],
+  }
+  try {
+    statement(invoice, plays)
+  } catch (e) {
+    t.is(e.message, 'unknown type: exception')
+  }
+
 });
 
 
