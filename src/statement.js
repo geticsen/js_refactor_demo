4c08554246b9c1c027e660fa335457d37b061e9d
ZHOUGR2 <286348794@qq.com>
2020-08-27 14:58:24 +0800

    [Refactor] [Green] refactor :extract usd method to format thisAmount

--- src/statement.js
+++ src/statement.js
@@ -1,4 +1,4 @@
-function geAmount(play,perf){
+function geAmount(play, perf) {
   let result = 0;
   switch (play.type) {
     case 'tragedy':
@@ -19,27 +19,30 @@ function geAmount(play,perf){
   }
   return result;
 }
+function usd(thisAmount) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(thisAmount / 100);
+}
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
+
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    const thisAmount = geAmount(play,perf);
+    const thisAmount = geAmount(play, perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
     //print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    result += ` ${play.name}: ${usd(thisAmount)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
   return result;
 }
